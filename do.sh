7c3944a55a56a328b45583d1cd607042559a0e22
JoeCordingley <joewcordingley@gmail.com>
2021-11-08 16:44:04 +0000

    pfft

--- do.sh
+++ do.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ln -s $(PWD)/.gitignore_global ~/.gitignore_global
-ln -s $(PWD)/.tmux.conf ~/.tmux.conf
-ln -s $(PWD)/.ctags ~/.ctags
+ln -s $PWD/.gitignore_global ~/.gitignore_global 
+ln -s $PWD/.tmux.conf ~/.tmux.conf
+ln -s $PWD/.ctags ~/.ctags
 mkdir -p ~/.config/nvim
-ln -s $(PWD)/.vimrc ~/.config/nvim/init.vim
+ln -s $PWD/.vimrc ~/.config/nvim/init.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
